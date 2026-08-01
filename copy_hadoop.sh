53459a86990d0289e2466fbd9d15b01d5f7c19df
Matthew Belanger <msb64@njit.edu>
2026-08-01 16:56:46 +0000

    Add copy_hadoop.sh

---
+++ copy_hadoop.sh
@@ -0,0 +1,18 @@
+# This script has been helpful for me to configure my Hadoop environment
+#
+set -eux
+
+for _host in slave1 slave2 slave3; do
+
+	rsync -ltr /etc/hosts $_host:/home/ubuntu/etc_hosts
+	ssh $_host "
+		sudo mkdir -p /usr/local/hadoop && \
+		sudo mkdir -p /usr/local/hadoop/data/dn && \
+		sudo chown -R ubuntu:ubuntu /usr/local/hadoop && \
+		sudo cp /home/ubuntu/etc_hosts /etc/hosts
+	"
+	rsync -lrt /usr/local/hadoop/ $_host:/usr/local/hadoop
+	rsync -ltr ~/.bashrc $_host:~/.bashrc
+done
+
+set +eux
